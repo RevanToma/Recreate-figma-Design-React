46e06e55316d1ecb57fb94ef3270fcc4fce037f4
RevanToma <tamrevan@gmail.com>
2023-02-20 16:44:02 +0100

    added /

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Recreate-figma-Design-React",
+  base: "/Recreate-figma-Design-React/",
   plugins: [react()],
 });
